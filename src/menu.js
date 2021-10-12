7c4c56b768807e360c7f81d7372eca4c54b70dbc
Arthur Bowen <arthur.bowen99@hotmail.com>
2021-10-12 10:44:55 -0300

    Initial commit

---
+++ src/menu.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./style.css";
+
+export default function Menu() {
+  return (
+    <div className="scrollmenu row dragscroll">
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://pm1.narvii.com/6858/dc719993a12098cf4ca0b5fa60ebb6ce711baa91v2_hq.jpg"
+          />
+          <p>Harry Potter e as Relíquias da Morte Parte 1</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://play-lh.googleusercontent.com/06bQR3NBl6wR3QBKruELmYru3rnQV75uCHFvZgf1D7WiGXlMHGG1mLAqXFT7SrfLyQQB"
+          />
+          <p>Harry Potter e as Relíquias da Morte Parte 2</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://br.web.img3.acsta.net/medias/nmedia/18/92/58/12/20207108.jpg"
+          />
+          <p>Star Wars Episódio II - Ataque do Clones</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://br.web.img3.acsta.net/medias/nmedia/18/90/95/62/20122008.jpg"
+          />
+          <p>De volta para o futuro</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://play-lh.googleusercontent.com/nz5s7-HJ3iijnvitzCwsa_oD5l7saKAnF_Hv3qGtsP6dtsXTqESZmfuQKqyZZD4UywyB"
+          />
+          <p>Vingadores Ultimato</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://br.web.img2.acsta.net/pictures/210/544/21054460_2013103118041242.jpg"
+          />
+          <p>Homem Aranha 2</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://clube-static.clubegazetadopovo.com.br/images/parceiros/1602268647844.png"
+          />
+          <p>Os Incríveis 2</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <img
+            alt=""
+            src="https://cdn.telecineplay.com.br/isl/api/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27177721%27&EntityType=%27Item%27&EntityId=%276201%27&Width=720&Height=1080&device=web_browser&subscriptions=Anonymous"
+          />
+          <p>Homem de Ferro 2</p>
+        </div>
+      </div>
+    </div>
+  );
+}
